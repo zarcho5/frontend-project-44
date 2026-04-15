3fb2d3a254a222437216aa584e993d893bda2e6d
zarcho5 <den812008@gmail.com>
2026-04-15 18:04:25 +0300

    shag 5.2

--- src/games/even.js
+++ src/games/even.js
@@ -1,26 +1,19 @@
 import readlineSync from 'readline-sync';
 
-const getRandomNumber = () => Math.floor(Math.random() * 100);
-
-const isEven = (num) => num % 2 === 0;
-
-export const runEvenGame = () => {
+export const runEvenGame = (name) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  const name = global.userName;
-
   for (let i = 0; i < 3; i += 1) {
-    const number = getRandomNumber();
+    const number = Math.floor(Math.random() * 100);
 
     console.log(`Question: ${number}`);
+
     const answer = readlineSync.question('Your answer: ');
 
-    const correctAnswer = isEven(number) ? 'yes' : 'no';
+    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
 
     if (answer !== correctAnswer) {
-      console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-      );
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
